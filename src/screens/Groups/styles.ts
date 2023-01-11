274c079864949df7f06cd932d8e87aa0415154f2
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-11 16:23:52 -0300

    :sparkles: feat: render screen in device safe area

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,7 +1,9 @@
+import { SafeAreaView } from "react-native";
+
 import styled from "styled-components/native";
 
-export const Container = styled.View`
-  flex: 1;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
-  padding: 24px;
+export const Container = styled.SafeAreaView`
+    flex: 1;
+    background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+    padding: 24px;
 `;
