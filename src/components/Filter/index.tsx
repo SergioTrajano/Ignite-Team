f44a2cd6d4b5c31fe1232a6f057a5f70bf28d2eb
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-11 04:27:43 -0300

    :sparkles: feat: create Filter component

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,19 @@
+import { TouchableOpacityProps } from "react-native";
+
+import { Container, FilterStyleProps, Title } from "./style";
+
+type Props = TouchableOpacityProps &
+    FilterStyleProps & {
+        title: string;
+    };
+
+export function Filter({ title, isActived = false, ...rest }: Props) {
+    return (
+        <Container
+            isActived={isActived}
+            {...rest}
+        >
+            <Title>{title}</Title>
+        </Container>
+    );
+}
