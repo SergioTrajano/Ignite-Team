497df974d33db95755fe556c209840c7618d7176
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-11 16:33:13 -0300

    :sparkles: feat: implements change of route

--- src/screens/NewGroups/index.tsx
+++ src/screens/NewGroups/index.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { HighLight } from "@components/Highlight";
@@ -5,6 +9,14 @@ import { Container, Content, Icon } from "./style";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+    const [group, setGroup] = useState<string>("");
+
+    const { navigate } = useNavigation<NativeStackNavigationProp<ReactNavigation.RootParamsList>>();
+
+    function handleNewGroup() {
+        navigate("players", { group });
+    }
+
     return (
         <Container>
             <Header showBackButton />
@@ -14,8 +26,15 @@ export function NewGroup() {
                     title="Nova turma"
                     subtitle="Cria a nova turma para adicionar os participantes"
                 />
-                <Input placeholder="Nome da turma" />
-                <Button title="Criar" />
+                <Input
+                    placeholder="Nome da turma"
+                    value={group}
+                    onChangeText={(text) => setGroup(text)}
+                />
+                <Button
+                    title="Criar"
+                    onPress={handleNewGroup}
+                />
             </Content>
         </Container>
     );
