6dc65621f437ef3dc1285949515ac67935a6e90d
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-11 16:26:10 -0300

    :sparkles: feat: render screen in device safe area

--- src/screens/NewGroups/style.ts
+++ src/screens/NewGroups/style.ts
@@ -1,6 +1,6 @@
 import { UsersThree } from "phosphor-react-native";
 import styled from "styled-components/native";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Container = styled.SafeAreaView`
     flex: 1;
