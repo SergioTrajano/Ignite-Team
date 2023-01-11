a6250a801710e6d6c87c3af5e54c7687756b9002
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-11 16:18:22 -0300

    :sparkles: feat: let user goBack to last screen onPress of icon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
-import { Container, Logo, BackButton, Icon } from "./style";
 import logoImg from "@assets/logo.png";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+import { Container, Logo, BackButton, Icon } from "./style";
 
 type Props = {
     showBackButton?: boolean;
 };
 
 export function Header({ showBackButton = false }: Props) {
+    const { navigate } = useNavigation<NativeStackNavigationProp<ReactNavigation.RootParamsList>>();
+
+    function handleGoBack() {
+        navigate("groups");
+    }
+
     return (
         <Container>
             {showBackButton && (
-                <BackButton>
+                <BackButton onPress={handleGoBack}>
                     <Icon />
                 </BackButton>
             )}
