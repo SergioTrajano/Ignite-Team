5420f7802251eee4e4e47476957637caade4e7f2
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-12 02:54:29 -0300

    :sparkles: feat: implements that user can create more than oue group

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,10 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { GROUP_COLLETION } from "@storage/storageConfig";
+import { groupGetAll } from "./groupGetAll";
 
 export async function groupCreate(newGroupName: string) {
     try {
-        await AsyncStorage.setItem(GROUP_COLLETION, newGroupName);
+        const storedGroup = await groupGetAll();
+
+        const storage = JSON.stringify([...storedGroup, newGroupName]);
+        await AsyncStorage.setItem(GROUP_COLLETION, storage);
     } catch (error) {
         throw error;
     }
