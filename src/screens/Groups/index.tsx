1288b3212e13ea2afb05d05ab740f03d5a97c157
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-12 05:30:37 -0300

    :sparkles: feat: add a loading component while fetching

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,21 @@
 import { useState, useCallback } from "react";
 import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { FlatList } from "react-native";
 
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { HighLight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { Loading } from "@components/Loading";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
 import { groupGetAll } from "@storage/group/groupGetAll";
 
 export function Groups() {
     const [groups, setGroups] = useState<string[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     const { navigate } = useNavigation<NativeStackNavigationProp<ReactNavigation.RootParamsList>>();
 
@@ -27,12 +30,16 @@ export function Groups() {
     );
 
     async function fetchGroup() {
+        setIsLoading(true);
+
         try {
             const data = await groupGetAll();
 
             setGroups(data);
         } catch (e) {
             console.log(e);
+        } finally {
+            setIsLoading(false);
         }
     }
 
@@ -47,21 +54,25 @@ export function Groups() {
                 title="Turmas"
                 subtitle="Jogue com a sua turma"
             />
-            <FlatList
-                data={groups}
-                keyExtractor={(item, i) => `${item} + ${i}`}
-                renderItem={({ item }) => (
-                    <GroupCard
-                        title={item}
-                        onPress={() => handleOpenGroup(item)}
-                    />
-                )}
-                contentContainerStyle={groups.length === 0 && { flex: 1 }}
-                ListEmptyComponent={() => (
-                    <ListEmpty message="Que tal cadastrar a primeira turma?" />
-                )}
-                showsVerticalScrollIndicator={false}
-            />
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <FlatList
+                    data={groups}
+                    keyExtractor={(item, i) => `${item} + ${i}`}
+                    renderItem={({ item }) => (
+                        <GroupCard
+                            title={item}
+                            onPress={() => handleOpenGroup(item)}
+                        />
+                    )}
+                    contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                    ListEmptyComponent={() => (
+                        <ListEmpty message="Que tal cadastrar a primeira turma?" />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                />
+            )}
 
             <Button
                 title="Criar nova turma"
