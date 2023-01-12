6c6ece72d14ec3e5476908f1a39411431931d097
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-12 04:53:50 -0300

    :sparkles: feat: implement functionalities of screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRoute } from "@react-navigation/native";
-import { FlatList } from "react-native";
+import { Alert, FlatList, TextInput } from "react-native";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./style";
 
@@ -12,6 +12,12 @@ import { Filter } from "@components/Filter";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { AppError } from "@utils/AppError";
+
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroupAndTeam } from "@storage/player/playerGetByGroupAndTeam";
+import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
+import { playerRemoveByGroup } from "@storage/player/playerRemoveByGroup";
 
 type RouteParams = {
     group: string;
@@ -19,10 +25,69 @@ type RouteParams = {
 
 export function Players() {
     const [activedTeam, setActivedteam] = useState<string>("TIME A");
-    const [players, setPlayers] = useState([]);
+    const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
+    const [newPlayerName, setNewPlayerName] = useState<string>("");
 
     const route = useRoute();
     const { group } = route.params as RouteParams;
+
+    const newPlayerNameInputRef = useRef<TextInput>(null);
+
+    async function handleAddPlayer() {
+        if (newPlayerName.trim() === "") {
+            console.log("To aqui");
+            return Alert.alert(
+                "Novo participante",
+                "Inclua o nome do participante para adicionar-lo."
+            );
+        }
+
+        const newPlayer = {
+            name: newPlayerName,
+            team: activedTeam,
+        };
+
+        try {
+            await playerAddByGroup(newPlayer, group);
+
+            newPlayerNameInputRef.current?.blur();
+            setNewPlayerName("");
+            await fetchPlayersByTeam();
+        } catch (error) {
+            if (error instanceof AppError) {
+                Alert.alert("Novo participante", error.message);
+            } else {
+                console.log(error);
+                Alert.alert("Novo participante", "Não foi possível adicionar o participante.");
+            }
+        }
+    }
+
+    async function handlePlayerRemove(name: string) {
+        try {
+            await playerRemoveByGroup(name, group);
+
+            await fetchPlayersByTeam();
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Excluir participante", `Não foi possível excluir o participante ${name}`);
+        }
+    }
+
+    async function fetchPlayersByTeam() {
+        try {
+            const playerByTeam = await playersGetByGroupAndTeam(group, activedTeam);
+
+            setPlayers(playerByTeam);
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Participante", "Não foi possível carregar os participantes.");
+        }
+    }
+
+    useEffect(() => {
+        fetchPlayersByTeam();
+    }, [activedTeam]);
 
     return (
         <Container>
@@ -35,11 +100,19 @@ export function Players() {
 
             <Form>
                 <Input
+                    inputRef={newPlayerNameInputRef}
                     placeholder="Nome do participante"
                     autoCorrect={false}
                     style={{ paddingTop: 20, paddingBottom: 0 }}
+                    value={newPlayerName}
+                    onChangeText={(text) => setNewPlayerName(text)}
+                    onSubmitEditing={handleAddPlayer}
+                    returnKeyType="done"
                 />
-                <ButtonIcon icon="add" />
+                <ButtonIcon
+                    icon="add"
+                    onPress={() => handleAddPlayer()}
+                />
             </Form>
 
             <HeaderList>
@@ -60,11 +133,11 @@ export function Players() {
 
             <FlatList
                 data={players}
-                keyExtractor={(item) => item}
+                keyExtractor={(item) => item.name}
                 renderItem={({ item }) => (
                     <PlayerCard
-                        name={item}
-                        onRemove={() => {}}
+                        name={item.name}
+                        onRemove={() => handlePlayerRemove(item.name)}
                     />
                 )}
                 ListEmptyComponent={() => <ListEmpty message="Não há pessoas nesse time" />}
